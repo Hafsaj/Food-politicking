ca2c09ac6bbae6b0c54d10d5e7a40155d73d421f
Hafsaj <32081620+Hafsaj@users.noreply.github.com>
2017-09-22 09:40:27 +0100

    styling and minor jquery

---
+++ _site/js/main.js
@@ -0,0 +1,33 @@
+//load the DOM
+$(document).ready(function(){
+
+  //Click the hamburger icon
+  $('.toggle-mobile-menu').click(function(){
+
+  //Show the mobile menu
+  $('#mobile_menu').toggleClass('mobile_menu_visible');
+
+});
+
+  //Click the cross icon
+
+  //Close the mobile menu
+
+  /*
+  ** Includes:
+  ** - jquery.js
+  ** - jquery.flexslider.js
+  */
+
+    // Assign flexslider functoin to class .flexslider
+    $('.flexslider').flexslider({
+      // Customize flexslider properties
+      animation: "slide",
+      slideshowSpeed: 5000,
+      animationSpeed: 1000,
+      slideshow: false,
+      directionNav: false
+
+    });
+
+});
